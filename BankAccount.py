d0627d5c795f7b8f14b3b54095ed84dad478dfc5
mastermicks <54526626+mastermicks@users.noreply.github.com>
2019-08-30 07:39:57 +0200

    Update BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -12,10 +12,10 @@ class BankAccount:
     return self.balance
 
   def getAccountHolder(self):
-    return accountHolder
+    return self.accountHolder
 
   def getDateOpened(self):
-    return dateOpened 
+    return self.dateOpened 
 
   def setAccountNo(self, newAccountNo):
     self.accountNo = newAccountNo
@@ -43,6 +43,10 @@ class BankAccount:
     return "Bank balance: " + self.balance 
 
 
+class BalanceError(Exception):
+  value = "Sorry, you only have " + str(BankAccount.balance) + "in your account."
+
+
 class SavingsAccount(BankAccount):
   def __init__(self, interestRate, dateLastInterestAdded, interestAccrued):
     self.interestRate = float(interestRate)
@@ -50,7 +54,7 @@ class SavingsAccount(BankAccount):
     self.interestAccrued = float(interestAccrued)
 
   def getInterestRate(self):
-    return self.interRate
+    return self.interestRate
 
   def getDateLastInterestAdded(self):
     return self.dateLastInterestAdded
@@ -67,11 +71,11 @@ class SavingsAccount(BankAccount):
   def setInterestAccrued(self, newInterestAccrued):
     self.interestAccrued = newInterestAccrued
 
-  def addInterest(self, date):
-    #finish interest class first
+  #def addInterest(self, date):
+    #super().balance += OurDate.numberOfDaysBetween(date1, otherDate) * 0.03
     
-  def __str__(self):
-    return "Savings account balance: " + str(self.balance)
+#  def __str__(self):
+#    return "Savings account balance: " + str(self.balance)
 
   
 class NoticeDepositAccount(SavingsAccount):
@@ -84,12 +88,9 @@ class NoticeDepositAccount(SavingsAccount):
   def setNoticePeriod(self, newNoticePeriod):
     self.noticePeriod = newNoticePeriod
 
-  
-class BalanceError(Exception):
-  value = "Sorry, you only have " + str(self.balance) + "in your account."
 
 
-class OurDate(self, dayOfMonth, month, year):
+class OurDate:
   def __init__(self, dayOfMonth, month, year):
     self.dayOfMonth = int(dayOfMonth)
     self.month = int(month)
@@ -101,5 +102,32 @@ class OurDate(self, dayOfMonth, month, year):
   def setOurDate(self, newDayOfMonth, newMonth, newYear):
     self.dayOfMonth = newDayOfMonth
 
-  def numberOfDaysBetween(self, date1, otherDate):
-    
+  @staticmethod
+  def numberOfDaysBetween(date1, otherDate):
+    months = [30, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    leapyears = [1992, 1996, 2000, 2004, 2008, 2012]
+    leapyearsbetween = []
+
+    year1 = int(date1[:4])
+    month1 = int(date1[4:6])
+    day1 = int(date1[6:8])
+
+    otherYear = int(otherDate[:4])
+    otherMonth = int(otherDate[4:6])
+    otherDay = int(otherDate[6:8])
+
+    for i in range(year1, otherYear-1, -1):
+        if i in leapyears:
+            leapyearsbetween.append(i)
+
+    numofdays1 = year1*365 + sum(months[:month1]) + day1
+    numofdays2 = otherYear*365 + sum(months[:otherMonth]) + otherDay
+
+    totaldays = numofdays1 - numofdays2 + len(leapyearsbetween)
+    print("Calculating number of days between", date1, "and", otherDate)
+    return totaldays
+
+class InterestAccount(BankAccount):
+    def deposit(self, amount):
+        BankAccount.deposit(self, amount)
+        self.balance *= 1.03
